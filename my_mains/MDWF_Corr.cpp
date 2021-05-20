78e9dc3bafd5383704e128e4d538c7a7325e3a8f
Tristan Ueding <trisueding@web.de>
2021-05-20 20:42:57 +0200

    Added a executable that can compute spatial and temporal meson correlators

---
+++ my_mains/MDWF_Corr.cpp
@@ -0,0 +1,221 @@
+/*If you use a sink from Module MSink the SMeson (spatial correlator) only works
+ for the sink SPoint since there is a slicing in z direction */
+
+#include <Hadrons/Application.hpp>
+#include <Hadrons/Modules.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+
+int main(int argc, char *argv[])
+{
+    // initialization //////////////////////////////////////////////////////////
+    Grid_init(&argc, &argv);
+    HadronsLogError.Active(GridLogError.isActive());
+    HadronsLogWarning.Active(GridLogWarning.isActive());
+    HadronsLogMessage.Active(GridLogMessage.isActive());
+    HadronsLogIterative.Active(GridLogIterative.isActive());
+    HadronsLogDebug.Active(GridLogDebug.isActive());
+    LOG(Message) << "Grid initialized" << std::endl;
+
+    // Set additional input parameters
+    // default values
+    std::vector<std::string> flavour;
+    std::vector<double>      mass;
+    int Ls_in=8;
+    double M5_in=1.8;
+    double b5_in=1.5;
+    double c5_in=0.5;
+    double ml_in=0.001;
+    double ms_in=0.01;
+    std::string path_conf_in = "" ;
+    std::string conf_name_in="unit";
+    double conf_number_in =  1000;
+    bool tdir_in = false ;
+    bool sdir_in = false ;
+    for(int i=0;i<argc;i++){
+          if(std::string(argv[i]) == "-Ls"){
+            std::stringstream ss(argv[i+1]); ss >> Ls_in;
+            }
+          if(std::string(argv[i]) == "-M5"){
+            std::stringstream ss(argv[i+1]); ss >> M5_in;
+            }
+          if(std::string(argv[i]) == "-b5"){
+            std::stringstream ss(argv[i+1]); ss >> b5_in;
+            }
+          if(std::string(argv[i]) == "-c5"){
+            std::stringstream ss(argv[i+1]); ss >> c5_in;
+            }
+          if(std::string(argv[i]) == "-conf_name"){
+            std::stringstream ss(argv[i+1]); ss >> conf_name_in;
+            }
+          if(std::string(argv[i]) == "-conf_number"){
+            std::stringstream ss(argv[i+1]); ss >> conf_number_in;
+            }
+          if(std::string(argv[i]) == "-ml"){
+            std::stringstream ss(argv[i+1]); ss >> ml_in;
+            flavour.push_back("l") ;
+            mass.push_back(ml_in) ;
+            }
+          if(std::string(argv[i]) == "-ms"){
+            std::stringstream ss(argv[i+1]); ss >> ms_in;
+            flavour.push_back("s") ;
+            mass.push_back(ms_in) ;
+            }
+          if(std::string(argv[i]) == "-path_conf"){
+            std::stringstream ss(argv[i+1]); ss >> path_conf_in;
+            }
+          if(std::string(argv[i]) == "-tdir"){
+              tdir_in = true;
+              }
+          if(std::string(argv[i]) == "-sdir"){
+                  sdir_in = true;
+                  }
+        }
+    if (!tdir_in && !sdir_in){
+      LOG(Message) << "You have to use at least -sdir or -tdir \
+      to set a direction for the contraction." << std::endl;
+      exit();
+    }
+    // Show additional input parameters
+    LOG(Message) << "Ls = " << Ls_in << std::endl;
+    LOG(Message) << "M5 = " << M5_in << std::endl;
+    LOG(Message) << "b5 = " << b5_in << std::endl;
+    LOG(Message) << "c5 = " << c5_in << std::endl;
+    //LOG(Message) << "ml = " << ml_in << std::endl;
+    //LOG(Message) << "ms = " << ms_in << std::endl;
+    for (int i; i< flavour.size() ; i++){
+      LOG(Message) << flavour[i] << " = " << mass[i] << std::endl;
+    }
+    LOG(Message) << "conf_path = " << path_conf_in << std::endl;
+    LOG(Message) << "conf_name = " << conf_name_in << std::endl;
+    LOG(Message) << "conf_number = " << conf_number_in << std::endl;
+    LOG(Message) << "calculate temporal correlator = " << tdir_in << std::endl;
+    LOG(Message) << "calculate spatial correlator = " << sdir_in << std::endl;
+
+
+
+    // run setup ///////////////////////////////////////////////////////////////
+    Application              application;
+    //std::vector<std::string> flavour = {"l", "s" };
+    //std::vector<double>      mass    = {ml_in, ms_in };
+
+    // global parameters
+    Application::GlobalPar globalPar;
+    globalPar.trajCounter.start = conf_number_in;
+    globalPar.trajCounter.end   = conf_number_in + 1;
+    globalPar.trajCounter.step  = 1;
+    globalPar.runId             = conf_name_in;
+    application.setPar(globalPar);
+    // gauge field
+
+    if(conf_name_in != "unit"){
+        LOG(Message) << "Use nersc configuration" << std::endl;
+	MIO::LoadNersc::Par loadnerscpar;
+        // the dot and confnumber are added in the LoadNersc module
+        loadnerscpar.file =  path_conf_in + "/" + conf_name_in;
+        //std::cout << loadnerscpar.file << std::endl;
+        application.createModule<MIO::LoadNersc>("gauge",loadnerscpar);
+
+    }
+    else
+    {
+       LOG(Message) << "Use unit configuration" << std::endl;
+       application.createModule<MGauge::Unit>("gauge");
+    }
+
+    // sources
+    MSource::Point::Par ptPar;
+    ptPar.position = "0 0 0 0";
+    application.createModule<MSource::Point>("pt", ptPar);
+    // sink
+    MSink::SPoint::Par ssinkPar;
+    ssinkPar.mom = "0 0 0";
+    application.createModule<MSink::ScalarSPoint>("ssink", ssinkPar);
+
+    // set fermion boundary conditions to be periodic space, antiperiodic time.
+    std::string boundary = "1 1 1 -1";
+    std::string twist = "0. 0. 0. 0.";
+
+    for (unsigned int i = 0; i < flavour.size(); ++i)
+    {
+        // actions
+        MAction::MobiusDWF::Par actionPar;
+        actionPar.gauge = "gauge";
+        actionPar.Ls    = Ls_in ;
+        actionPar.M5    = M5_in ;
+        actionPar.b     = b5_in ;
+        actionPar.c     = c5_in ;
+        actionPar.mass  = mass[i];
+        actionPar.boundary = boundary;
+        actionPar.twist = twist;
+        application.createModule<MAction::MobiusDWF>("MobiusDWF_" + flavour[i], actionPar);
+
+        // solvers
+        MSolver::RBPrecCG::Par solverPar;
+        solverPar.action       = "MobiusDWF_" + flavour[i];
+        solverPar.residual     = 1.0e-16;
+        solverPar.maxIteration = 10000;
+        application.createModule<MSolver::RBPrecCG>("CG_" + flavour[i],
+                                                    solverPar);
+
+        // propagators
+        MFermion::GaugeProp::Par quarkPar;
+        quarkPar.solver = "CG_" + flavour[i];
+        quarkPar.source = "pt";
+        application.createModule<MFermion::GaugeProp>("Qpt_" + flavour[i], quarkPar);
+    }
+    for (unsigned int i = 0; i < flavour.size(); ++i)
+    for (unsigned int j = i; j < flavour.size(); ++j)
+    {
+
+    if (tdir){
+        // sink for the temporal direction
+        MSink::Point::Par tsinkPar;
+        tsinkPar.mom = "0 0 0";
+        application.createModule<MSink::ScalarPoint>("tsink", tsinkPar);
+
+        //Contraction in the spatial direction
+        MContraction::Meson::Par mesPar;
+
+        tmesPar.output  = "tmesons/pt_" + flavour[i] + flavour[j] + "_" + conf_name_in ;
+        tmesPar.q1      = "Qpt_" + flavour[i];
+        tmesPar.q2      = "Qpt_" + flavour[j];
+        tmesPar.gammas  = "all";
+        tmesPar.sink    = "tsink";
+        application.createModule<MContraction::Meson>("tmeson_pt_"
+                                                      + flavour[i] + flavour[j],
+                                                      tmesPar);
+      }
+
+    if (sdir){
+        // sink for the spatial direction
+        MSink::SPoint::Par ssinkPar;
+        ssinkPar.mom = "0 0 0";
+        application.createModule<MSink::ScalarSPoint>("ssink", ssinkPar);
+
+        //Contraction in the spatial direction
+        MContraction::SMeson::Par smesPar;
+
+        smesPar.output  = "smesons/pt_" + flavour[i] + flavour[j] + "_" + conf_name_in ;
+        smesPar.q1      = "Qpt_" + flavour[i];
+        smesPar.q2      = "Qpt_" + flavour[j];
+        smesPar.gammas  = "all";
+        smesPar.sink    = "ssink";
+        application.createModule<MContraction::SMeson>("smeson_pt_"
+                                                      + flavour[i] + flavour[j],
+                                                      smesPar);
+      }
+
+    }
+
+    // execution
+    application.saveParameterFile("smeson_spectrum.xml");
+    application.run();
+
+    // epilogue
+    LOG(Message) << "Grid is finalizing now" << std::endl;
+    Grid_finalize();
+
+    return EXIT_SUCCESS;
+}
